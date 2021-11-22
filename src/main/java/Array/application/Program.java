426e63be1b7a9d7f4ccc241825eb02d8568a17d5
maikcosta@gmail.com <Mk1311cts>
2021-11-22 08:05:40 -0300

    Implements try catch

--- src/main/java/Array/application/Program.java
+++ src/main/java/Array/application/Program.java
@@ -2,35 +2,48 @@ package Array.application;
 
 import Array.entities.Product;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
+    private static Product[] vect;
+    private static Double avg;
+    private static Double sum;
+
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         System.out.println("How many products");
-        int n = sc.nextInt();
+        Integer n = 0 ;
+        try{
+            n = sc.nextInt();
 
-        Product[] vect = new Product[n];
+            Product[] vect = new Product[n];
 
-        for (int i=0; i<vect.length; i++){
-            sc.nextLine();
-            int item = i +1;
-            System.out.println(item +" - Insert product name");
-            String name = sc.nextLine();
-            System.out.println(item + " - Insert product value (EX: 900.00)");
-            double price = sc.nextDouble();
-            vect[i] = new Product(name,price);
-        }
+            for (int i=0; i<vect.length; i++){
+                sc.nextLine();
+                int item = i +1;
+                System.out.println(item +" - Insert product name");
+                String name = sc.nextLine();
+                System.out.println(item + " - Insert product value (EX: 900.00)");
+                double price = sc.nextDouble();
+                vect[i] = new Product(name,price);
+            }
+        sum = 0.0;
 
-        double sum = 0.0;
-        for (int i=0; i<vect.length; i++){
+        for (int i=0; i<n; i++){
             sum += vect[i].getPrice();
         }
-        double avg = sum / vect.length;
-
-        System.out.printf("AVERAGE PRICE = %.2f%n", avg);
+        avg = sum / n;
+        }catch(InputMismatchException e){
+            System.out.println("Please insert number only " + e.getMessage());
+        }catch(NullPointerException e){
+            System.out.println("Null" + e.getMessage());
+        }
+        if(avg != null){
+            System.out.printf("AVERAGE PRICE = %.2f%n", avg);
+        }
 
         sc.close();
     }
